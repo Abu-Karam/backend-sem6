26c8c0686b6029db621a57b41cf97826c84c8648
Abu Karam <abu.karam@outlook.dk>
2020-04-23 00:28:19 +0200

    put post

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var logger = require('morgan');
 app.use(cors());
 app.use(logger('dev'));
 app.use(jsonParse());
-
+// get all meals
 app.get('/meals', function(req, res, next){
     db.query("SELECT * FROM meal", (err, rows, fields) => {
         if(!err)
@@ -22,6 +22,8 @@ app.get('/meals', function(req, res, next){
     })
 });
 
+
+//get one meal
 app.get('/meals/:id', function(req, res, next){
     db.query("SELECT * FROM meal WHERE mealId = ?", [req.params.id],(err, rows, fields) => {
         if(!err)
@@ -31,6 +33,8 @@ app.get('/meals/:id', function(req, res, next){
     })
 });
 
+
+//delete one meal
 app.delete('/meals/:id', function(req, res, next){
     db.query("DELETE FROM meal WHERE mealId = ?", [Number(req.params.id)],(err, rows, fields) => {
         if(!err)
@@ -40,8 +44,31 @@ app.delete('/meals/:id', function(req, res, next){
     })
 });
 
+
+//create meal
 app.post('/meals', function(req, res, next){
-})
+    if (!req.body) {
+        res.status(400).send({
+          message: "Content can not be empty!"
+        });
+      }
+      dp.query('INSERT INTO meal SET ?', req.body, (error, result) => {
+        if (error) throw error;
+ 
+        res.status(201).send(` added with ID: ${result.insertId}`);
+    });
+});
+
+//Update meal
+app.put('/meals/:id', function(req, res, next){
+    const id = req.params.id;
+ 
+    db.query('UPDATE meal SET ? WHERE id = ?', [req.body, id], (error, result) => {
+        if (error) throw error;
+ 
+        res.send('User updated successfully.');
+    });
+});
 
 
 var port = process.env.port || 3000;
